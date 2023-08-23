e0a34b32640889ac65bd76ba6c55770ee343705c
xkyii <xkyii@qq.com>
2023-08-23 11:39:37 +0800

    Add: Ks.Exp.Wino Win32操作

---
+++ exp/Ks.Exp.Wino/Program.cs
@@ -0,0 +1,15 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+// 寻找指定的窗口句柄
+var h1 = PInvoke.FindWindow("Notepad", null);
+var h2 = PInvoke.FindWindowEx(h1, HWND.Null, "Edit", null);
+
+// 看下找到没
+Console.WriteLine($"h1: 0x{h1.Value.ToString("X8")}");
+Console.WriteLine($"h2: 0x{h2.Value.ToString("X8")}");
+
+// 发送滚动消息
+PInvoke.SendMessage(h2, PInvoke.WM_VSCROLL, (WPARAM) (int) SCROLLBAR_COMMAND.SB_PAGEUP, (LPARAM)0);
+PInvoke.SendMessage(h2, PInvoke.WM_VSCROLL, (WPARAM) (int) SCROLLBAR_COMMAND.SB_PAGEDOWN, (LPARAM)0);
