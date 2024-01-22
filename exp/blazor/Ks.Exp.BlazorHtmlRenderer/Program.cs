919fb458ac5a7e6ca0bfeba756de6acba2df8466
xkyii@th <xkyii@foxmail.com>
2024-01-22 14:22:02 +0800

    Add: Ks.Exp.BlazorHtmlRenderer

---
+++ exp/blazor/Ks.Exp.BlazorHtmlRenderer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Ks.Exp.BlazorHtmlRenderer;
+
+IServiceCollection services = new ServiceCollection();
+services.AddLogging();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+{
+    var dictionary = new Dictionary<string, object?>
+    {
+        { "Message", "Hello from the Render Message component!" }
+    };
+
+    var parameters = ParameterView.FromDictionary(dictionary);
+    var output = await htmlRenderer.RenderComponentAsync<RenderMessage>(parameters);
+
+    return output.ToHtmlString();
+});
+
+Console.WriteLine(html);
