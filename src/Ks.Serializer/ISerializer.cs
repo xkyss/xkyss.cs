a3d11e36fae6aac7f3c8b5391c9c3499c94a848b
xkyii@th <xkyii@foxmail.com>
2024-02-22 18:00:44 +0800

    Add: Ks.Serialize

---
+++ src/Ks.Serializer/ISerializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Buffers;
+
+namespace Ks.Serializer;
+
+public interface ISerializer
+{
+    public void Serialize<T>(Stream stream, T value);
+
+    public byte[] Serialize<T>(T value);
+
+    public T Deserialize<T>(byte[] data);
+
+    public T Deserialize<T>(Stream data);
+
+    public T Deserialize<T>(ReadOnlySequence<byte> data);
+}
