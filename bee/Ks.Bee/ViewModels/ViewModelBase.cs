17418cad053fd76607cb71c37c3ea3fc0894a98c
xkyii@th <xkyii@foxmail.com>
2024-10-29 11:28:53 +0800

    Add: Ks.Bee

---
+++ bee/Ks.Bee/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ks.Bee.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ bee/Ks.Bee/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Ks.Bee.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
