5de13cafc5810aabc3b1dde72b09240935ef9f77
xkyii@th <xkyii@foxmail.com>
2024-12-09 17:01:20 +0800

    Up: Pathx

---
+++ src/Ks.Core/KsConstants.cs
@@ -0,0 +1,7 @@
+namespace Ks.Core
+{
+    public static class KsConstants
+    {
+        public const string ProfilePath = ".xky";
+    }
+}
--- src/Ks.Core/Utilities/System/IO/Pathx.cs
+++ src/Ks.Core/Utilities/System/IO/Pathx.cs
@@ -8,4 +8,32 @@ public static class Pathx
     {
         return Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? Directory.GetCurrentDirectory();
     }
+
+    public static void EnsureDirectoryExists(string directory)
+    {
+        // 检查目录是否存在
+        if (!Directory.Exists(directory))
+        {
+            // 如果目录不存在，则创建它
+            Directory.CreateDirectory(directory);
+        }
+    }
+
+    public static string Profile(string project)
+    {
+        return Profile(KsConstants.ProfilePath, project);
+    }
+
+    public static string Profile(string xky, string project)
+    {
+        // 获取当前用户的用户文件夹路径
+        var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var specificPath = Path.Combine(path, xky, project);
+        // 如果子文件夹不存在，则创建它
+        if (!Directory.Exists(specificPath))
+        {
+            Directory.CreateDirectory(specificPath);
+        }
+        return specificPath;
+    }
 }
