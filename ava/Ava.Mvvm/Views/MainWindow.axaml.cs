4890f610f42034e6687f2dd1d5f648785f6ba464
xkyii@th <xkyii@foxmail.com>
2025-08-05 11:31:31 +0800

    Up: 无边框

--- ava/Ava.Mvvm/Views/MainWindow.axaml.cs
+++ ava/Ava.Mvvm/Views/MainWindow.axaml.cs
@@ -1,8 +1,8 @@
-using Avalonia.Controls;
+using Ursa.Controls;
 
 namespace Ava.Mvvm.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : UrsaWindow
 {
     public MainWindow()
     {
