0316b1f3fd92fd286604e838de09392ed5fc854d
xkyii@th <xkyii@foxmail.com>
2025-08-05 11:51:52 +0800

    Add: 标题栏右侧按钮(组)

---
+++ ava/Ava.Mvvm/Views/TitleBarRightContent.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Ava.Mvvm.Views
+{
+    public partial class TitleBarRightContent : UserControl
+    {
+        public TitleBarRightContent()
+        {
+            InitializeComponent();
+        }
+        
+        private async void OpenRepository(object? sender, RoutedEventArgs e)
+        {
+            var top = TopLevel.GetTopLevel(this);
+            if (top is null)
+            {
+                return;
+            }
+            var launcher = top.Launcher;
+            await launcher.LaunchUriAsync(new Uri("https://github.com/xkyss/xkyss.cs"));
+        }
+    }
+}
